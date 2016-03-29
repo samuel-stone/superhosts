39a359026937e828a2ccd6f364174ecb474db764
Sam Stone <samuelhenrystone@gmail.com>
2016-03-29 02:39:28 -0700

    superhosts v1.0
    Built prototype with nav, jumbotron, test grid ft. super host drayman
    green

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('.grid').isotope({
+    // options
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows'
+  });
+
+  // init Isotope
+var $grid = $('.grid').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+});
